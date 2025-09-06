359b68b5c4dc7b9bab97cf93ac90a3b3f8c64e25
Sam Brown <sambrown.w@gmail.com>
2025-09-06 22:48:06 +0930

    a

--- lua/modules/inv_watcher.lua
+++ lua/modules/inv_watcher.lua
@@ -39,7 +39,7 @@ function inv_watcher.tick()
     local inv_slot=1
     local inv_size=rb.inventorySize()
     for item in ic.getAllStacks(in_side) do
-        if item then
+        if item and #item>0 then
             for i=inv_slot, inv_size do
                 if not utils.is_reserved(i) and rb.count(i)==0 then
                     inv_slot=i
@@ -49,7 +49,7 @@ function inv_watcher.tick()
             end
         end
     end
-    
+
     return
 end
 
